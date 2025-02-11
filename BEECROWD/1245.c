7a4c84be2f39a5a98a34b2a1aa72b4189237bf88
MarceloGSFh <marcelo.souzafh1@gmail.com>
2025-02-11 20:41:28 -0300

    1245.c

---
+++ BEECROWD/1245.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define TAM_MAX 61
+
+int main() {
+    int total_b, tam, par=0;
+    char l;
+    int b[TAM_MAX][2];
+    while (scanf("%d", &total_b) != EOF) {
+        for (int i = 30; i <= 60; i++) {
+            b[i][0] = 0;
+            b[i][1] = 0;
+        }
+        for (int i = 0; i < total_b; i++) {
+            scanf("%d %c", &tam, &l);
+            if (lado == 'E') {
+                b[tam][0]++;
+            } else if (l == 'D') {
+                b[tam][1]++;
+            }
+        }
+        for (int i = 30; i <= 60; i++) {
+            par += (b[i][0] < b[i][1]) ? b[i][0] : b[i][1];
+        }
+        printf("%d\n", par);
+    }
+    return 0;
+}
